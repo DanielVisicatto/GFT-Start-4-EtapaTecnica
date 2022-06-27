98f98cddbe28b6ac02c6f55a378a6f5ae3e209d6
Daniel Visicatto <dpfreestyle@gmail.com>
2022-06-27 07:44:46 -0300

    Variáveis desnecessárias removidas

--- Atividade4/Program.cs
+++ Atividade4/Program.cs
@@ -1,22 +1,20 @@
 ﻿
 bool isValid = false;
-int inicial = 0, final = 0, count = 0;
-
 
 while (isValid == false)
 {
     Console.WriteLine("Informe um número inicial maior ou igual a 3 (tres)");
-    inicial = int.Parse(Console.ReadLine());
+    int inicial = int.Parse(Console.ReadLine());
 
     Console.WriteLine("Informe um número final maior que o número inicial e menor que 999");
-    final = int.Parse(Console.ReadLine());
+    int final = int.Parse(Console.ReadLine());
 
     if (inicial >= 3 && final > inicial && final < 999)
     {
 
         for (int i = inicial; i <= final; i++)
         {
-            count = 0;
+            int count = 0;
             for (int j = 2; j < i; j++)
             {
                 if (i % j == 0)
